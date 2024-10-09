c768131475cf2f45d240682a7699b5b476ceb294
love-quinn <lucaslucal75@gmail.com>
2024-10-09 16:10:06 -0300

    feat: included dashboard link on sidebar

--- src/app/(admin)/dashboard/components/sidebar.tsx
+++ src/app/(admin)/dashboard/components/sidebar.tsx
@@ -21,10 +21,17 @@ const Sidebar = () => {
       </h1>
 
       <div className="flex w-full flex-col gap-3">
-        <Button variant="outline" className="flex w-full justify-start gap-2">
+      <Link href="/dashboard">
+        <Button variant="outline" className={`flex w-full justify-start gap-2
+          ${
+            path === "/dashboard" &&
+            "bg-primary text-white hover:bg-primary"
+          }
+        `}>
           <LayoutDashboardIcon size={16} />
           Dashboard
         </Button>
+      </Link>
 
         <Link href="/dashboard/products">
           <Button
